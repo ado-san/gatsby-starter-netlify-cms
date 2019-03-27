527dc1c209c0c47db5a81553542fa5a80ba88070
Adrien <adrien@Adriens-MBP.setup.net>
2019-03-27 16:57:48 +0800

    bigger logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="ADVentures" style={{ width: "200px" }} />
+              <img src={logo} alt="ADVentures" style={{ width: "300px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
